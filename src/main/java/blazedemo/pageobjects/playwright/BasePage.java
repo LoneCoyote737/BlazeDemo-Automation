54a9fbdc296cc6b6abbca86bc1f3d06193711661
John Manuel <sp.john.manuel737@gmail.com>
2026-04-19 16:52:22 +0530

    BaseTest, BasePage done

--- src/main/java/blazedemo/pageobjects/playwright/BasePage.java
+++ src/main/java/blazedemo/pageobjects/playwright/BasePage.java
@@ -1,5 +1,30 @@
 package blazedemo.pageobjects.playwright;
 
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.AriaRole;
+
+/**
+ * To be inherited by all playwright page object classes.<br/>
+ * Contains useful {@code Locator} finding functions.
+ */
 public class BasePage {
 
+    protected Page page;
+    public BasePage(Page page) {
+        this.page = page;
+    }
+
+    protected Locator locateAriaElement (String elementName, AriaRole role) {
+        return page.getByRole(role, new Page.GetByRoleOptions().setName(elementName));
+    }
+    protected Locator locateButton (String buttonName) {
+        return locateAriaElement(buttonName, AriaRole.BUTTON);
+    }
+    protected Locator locatedTextbox (String textboxName) {
+        return locateAriaElement(textboxName, AriaRole.TEXTBOX);
+    }
+
+    protected Locator getByText (String text) {
+        return page.getByText(text);
+    }
 }
--- src/test/java/blazedemo/tests/playwright/BaseTest.java
+++ src/test/java/blazedemo/tests/playwright/BaseTest.java
@@ -1,4 +1,50 @@
 package blazedemo.tests.playwright;
 
+import com.microsoft.playwright.*;
+import org.testng.annotations.*;
+import java.util.List;
+
+/**
+ * To be inherited by all playwright Test script classes.<br/>
+ * Contains the setups that happen before and after a test runs.
+ */
 public class BaseTest {
+
+    protected static Playwright playwright;
+    protected static Browser browser;
+    protected Page page;
+    protected BrowserContext browserContext;
+    protected Browser.NewContextOptions newContextOptions;
+    protected String baseUrl;
+
+    public BaseTest (String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    @BeforeSuite(alwaysRun = true)
+    public void beforeSuite() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(
+                new BrowserType.LaunchOptions()
+                        .setHeadless(false)
+                        .setArgs(List.of("--start-maximized"))
+        );
+    }
+    @BeforeMethod(alwaysRun = true)
+    public void beforeMethod() {
+        newContextOptions = new Browser.NewContextOptions().setViewportSize(null);
+        browserContext = browser.newContext(newContextOptions);
+        page = browserContext.newPage();
+        page.navigate(baseUrl);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void afterMethod() {
+        browserContext.close();
+    }
+    @AfterSuite(alwaysRun = true)
+    public void afterSuite() {
+        browser.close();
+        playwright.close();
+    }
 }
